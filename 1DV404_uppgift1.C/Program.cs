f013ffd0ebb6e110cf48e8a3eb6b95d7615b7ba5
topcom94 <topcom94@hotmail.com>
2014-11-20 16:23:28 +0100

    färdig med 1.C

---
+++ 1DV404_uppgift1.C/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV404_uppgift1.C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountOfNumbers = 10;
+
+            int nextHighest = 0;
+            int highest = 0;
+            int userInput;
+            int i = 0;
+
+            do 
+            {
+                try{
+
+                    Console.Write("Tal {0}: ", i + 1);
+                    userInput = int.Parse(Console.ReadLine());
+                    if (i == 0)
+                    {
+
+                        highest = userInput;
+                        nextHighest = userInput;
+
+                    }
+                    else if (userInput == highest && nextHighest == highest)
+                    {
+                        nextHighest = highest;
+                        highest = userInput;
+                    }
+                    else
+                    {
+                        if (userInput > highest)
+                        {
+                            nextHighest = highest;
+                            highest = userInput;
+                        }
+                        else if (userInput > nextHighest && userInput < highest || userInput < nextHighest &&nextHighest == highest)
+                        {
+                            nextHighest == userInput;
+                        }
+                     }
+                    i++;
+                }
+                catch(Exception)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Skriv en giltig siffra!");
+                    Console.ResetColor();
+
+                }
+            }while (i < amountOfNumbers);
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            if (highest == nextHighest)
+            {
+                Console.WriteLine("Det finns ingen siffra som är högre än den högsta!");
+            }
+                else
+                Console.WriteLine("näst störst: {0}", nextHighest);
+                Console.ResetColor();
+            }
+        }
+    }
+}
