64ed93b533c2e8798db257e2d20698bddeec8cc9
topcom94 <topcom94@hotmail.com>
2014-11-20 17:23:15 +0100

    Klar med 3.A

---
+++ 1DV404uppgift3.A/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV404uppgift3.A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Skrev en textrad :)");
+                    userInput = Console.ReadLine().ToLower();
+                    if (string.IsNullOrEmpty(userInput))
+                    {
+                        throw new FormatException();
+                    }
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Du inte in någonting i raden!");
+                    Console.ResetColor();
+                }
+            }
+            bool isPalindrome = false;
+
+            char[] charArray = userInput.ToCharArray();
+            Array.Reverse(charArray);
+
+            if (new string(charArray) == userInput)
+            {
+                isPalindrome = true;
+            }
+
+            if (isPalindrome)
+            {
+                Console.WriteLine("Texten är Palindrom!");
+            }
+            else
+            {
+                Console.WriteLine("Texten är inte Palidrom!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
